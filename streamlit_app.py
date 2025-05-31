9d5ea7801b905a04279a910d3b92bc5b8d9d40c3
Srinivasa-Hexaware <136800591+Srinivasa-Hexaware@users.noreply.github.com>
2025-05-31 20:32:35 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,11 +34,12 @@ if ingredients_list:
     
     time_to_insert = st.button('Submit Order')
 
-  smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-  st.text(smoothiefroot_response)
-
     
     
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success(f'Your Smoothie is ordered {name_on_order}!', icon="✅")
+
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+        # st.text(smoothiefroot_response.json())
+        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
